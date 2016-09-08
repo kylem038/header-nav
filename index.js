f6d1c2a2592377c35d40473300675e0bed9cf02f
kylem038 <misenckj@gmail.com>
2016-09-07 19:50:04 -0600

    Base accordion functionality added

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ $dropdown = document.querySelector('.dropdown');
 $dropdown.addEventListener('click', function() {
   document.querySelector('.link-list').classList.toggle('show');
 });
+
+$('.js-accordion-trigger').bind('click', function(e){
+  jQuery(this).parent().find('.submenu').slideToggle('fast');
+ // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-expanded');
+  e.preventDefault();
+});
